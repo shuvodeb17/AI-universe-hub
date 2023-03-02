6f3c63d0de165f6ce5df7a2853861691fbcaf8bd
shuvodeb17 <shuvodeb17014@gmail.com>
2023-03-02 07:24:06 -0800

    API data load

---
+++ script/app.js
@@ -0,0 +1,7 @@
+const loadData = () => {
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data.tools));
+};
+loadData();
