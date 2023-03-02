9aa6528d7d54e61bae3b17669280ba7f6b01daaf
shuvodeb17 <shuvodeb17014@gmail.com>
2023-03-02 08:29:33 -0800

    Data has been shown on the card

--- script/app.js
+++ script/app.js
@@ -10,29 +10,31 @@ const loadData = () => {
 const displayData = (allData) => {
   const cardContainer = document.getElementById("card-container");
   allData.forEach((data) => {
+    const { name, image, features, published_in } = data;
+
     const createElement = document.createElement("div");
     createElement.innerHTML = `
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                <img class="rounded-t-lg p-5" src="${image}" alt="" />
             </a>
             <div class="p-5">
                 <a href="#">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Features</h5>
                 </a>
                 <ol class="list-decimal list-inside">
-                    <li>Natural language processing</li>
-                    <li>Contextual understanding</li>
-                    <li>Text generation</li>
+                    <li>${features[0] ? features[0] : "Not found"}</li>
+                    <li>${features[1] ? features[1] : "Not found"}</li>
+                    <li>${features[2] ? features[2] : "Not found"}</li>
                 </ol>
 
                 <hr class="mt-3 mb-3">
 
                 <div id="card-footer" class="flex items-center justify-between">
                     <div class="left">
-                        <h2 class="text-2xl font-bold">ChatGPT</h2>
+                        <h2 class="text-2xl font-bold">${name}</h2>
                         <i class="fa-regular fa-calendar-days mr-2 mt-2"></i>
-                        <p class="inline">11/01/2022</p>
+                        <p class="inline">${published_in}</p>
                     </div>
                     <div class="right">
                         <div class="bg-[#FEF7F7] h-10 w-10 flex items-center justify-center rounded-full cursor-pointer">
